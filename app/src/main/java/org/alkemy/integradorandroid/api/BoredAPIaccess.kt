baa9c5639a44859c4c18755b69f5a1526026cfbe
LuisRubenGrez <luis.grez@mercadolibre.com>
2022-06-08 18:05:32 -0300

    Activities List screen fixed and pull from develop-2

--- app/src/main/java/org/alkemy/integradorandroid/api/BoredAPIaccess.kt
+++ app/src/main/java/org/alkemy/integradorandroid/api/BoredAPIaccess.kt
@@ -1,16 +1,16 @@
 package org.alkemy.integradorandroid.api
 
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 class BoredAPIaccess {
-    var apiUrl = "http://www.boredapi.com/api/activity"
 
     // Access API on the 'random activities' endpoint:
     fun getBoredAPI_random(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("$apiUrl/")
+            .baseUrl("$BASE_URL/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -18,9 +18,22 @@ class BoredAPIaccess {
     // Access API on the 'category' endpoint:
     fun getBoredAPI_category(category: String): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("$apiUrl?type=$category")
+            .baseUrl("$BASE_URL?type=$category")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+
+ /*   val webService: WebService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build().create(WebService::class.java)
+    }*/
+
+    companion object{
+        const val BASE_URL = "http://www.boredapi.com/api/activity"
     }
 
 }
--- app/src/main/java/org/alkemy/integradorandroid/ui/activitieslist/ListActivity.kt
+++ app/src/main/java/org/alkemy/integradorandroid/ui/activitieslist/ListActivity.kt
@@ -1,7 +1,6 @@
 package org.alkemy.integradorandroid.ui.activitieslist
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import org.alkemy.integradorandroid.R
 import org.alkemy.integradorandroid.databinding.ActivityListBinding
@@ -10,9 +9,8 @@ class ListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
