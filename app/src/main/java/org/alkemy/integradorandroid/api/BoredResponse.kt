5a36347d48ab0acaa91f15d5419c50cb59db4f84
Dennis Mayr <dmayr.dev@gmail.com>
2022-06-08 14:09:27 -0400

    BoredResponse: added @SerializedName annotations

--- app/src/main/java/org/alkemy/integradorandroid/api/BoredResponse.kt
+++ app/src/main/java/org/alkemy/integradorandroid/api/BoredResponse.kt
@@ -4,11 +4,18 @@ import com.google.gson.annotations.SerializedName
 import retrofit2.http.Url
 
 data class BoredResponse(
+    @SerializedName("activity")
     var activity: String,
+    @SerializedName("type")
     var type: String,
+    @SerializedName("participants")
     var participants: Int,
+    @SerializedName("price")
     var price: Float,
+    @SerializedName("link")
     var link: Url,
+    @SerializedName("key")
     var key: Int,
+    @SerializedName("accesibility")
     var accessibility: Float
 )
