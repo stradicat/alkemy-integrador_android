f8835702731b85b443c7f36c0ac173082d0e2700
Dennis Mayr <dmayr.dev@gmail.com>
2022-06-08 14:43:07 -0400

    BoredResponse: added rudimentary API consumption functions (random and by category)

--- app/src/main/java/org/alkemy/integradorandroid/api/BoredAPIaccess.kt
+++ app/src/main/java/org/alkemy/integradorandroid/api/BoredAPIaccess.kt
@@ -3,12 +3,22 @@ package org.alkemy.integradorandroid.api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class BoredAPIaccess {
+    var apiUrl = "http://www.boredapi.com/api/activity"
+
     // Access API on the 'random activities' endpoint:
-    fun getBoredAPI_randomResponse(): Retrofit {
-        val apiUrl = "http://www.boredapi.com/api/activity/"
+    fun getBoredAPI_random(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(apiUrl)
+            .baseUrl("$apiUrl/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // Access API on the 'category' endpoint:
+    fun getBoredAPI_category(category: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("$apiUrl?type=$category")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
